5477b41a9b0d7c4a760d36609838efe8787a4716
bonohub13 <ken.saito.0813@gmail.com>
2021-07-04 21:31:19 +0900

    Added a wrapper to include all of the  headers for GameUtils library

---
+++ include/gameUtils/include/GameUtils.hpp
@@ -0,0 +1,9 @@
+#ifndef __GAMEUTILS_GAMEUTILS_HPP__
+#define __GAMEUTILS_GAMEUTILS_HPP__
+
+#include "score.hpp"
+#include "utils.hpp"
+#include "windowProfile.hpp"
+#include "racket.hpp"
+
+#endif // __GAMEUTILS_GAMEUTILS_HPP__
